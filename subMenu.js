3d64dea7f21829b0794fa92a9afd792cef6e4a3c
Natalie NSM722 <nataliembatha@gmail.com>
2021-11-02 17:11:49 +0100

    First commit

---
+++ subMenu.js
@@ -0,0 +1,9 @@
+/*In the below code, we handle the click event of out .btn, 
+selected the .submenu element and opened/closed it using the fadeToggle method, 
+providing 1s/1000ms for the animation speed.*/
+$(document).ready(function() {
+    $(alert('Document is ready!'))
+        $(".btn").click(function() {
+            $(".submenu").fadeToggle(1000)
+        });
+});
