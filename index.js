b65f57dfdd5d66afa2aee0789efac9433c0bfbee
Natalie <nataliembatha@gmail.com>
2023-04-26 09:42:34 +0200

    disabled the alert

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 selected the .submenu element and opened/closed it using the fadeToggle method, 
 providing 1s/1000ms for the animation speed.*/
 $(document).ready(function() {
-    $(alert('Document is ready!'))
+    // $(alert('Document is ready!'))
         $(".btn").click(function() {
             $(".submenu").fadeToggle(1000)
         });
